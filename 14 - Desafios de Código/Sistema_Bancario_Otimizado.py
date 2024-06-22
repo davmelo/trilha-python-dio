0160b10e8b16d9978a62a33e8396748d58e3af9b
davmelo <davfernando7@gmail.com>
2024-06-22 01:37:45 -0300

    criação da função verificar_cadastro_banco_conta() e melhoria nos print

--- 14 - Desafios de Código/Sistema_Bancario_Otimizado.py
+++ 14 - Desafios de Código/Sistema_Bancario_Otimizado.py
@@ -59,16 +59,18 @@ def cadastrar_conta(cpf):
     if not usuario_com_conta:
         conta_criada[cpf] = [{"agencia_conta": ("0001", numero_conta), "saldo": saldo, "extrato": extrato, "numero_saques": 0}]
         contas_banco.append(conta_criada)
+        return conta_criada[cpf][0]["agencia_conta"]
     else:
         conta_criada = {"agencia_conta": ("0001", numero_conta), "saldo": saldo, "extrato": extrato, "numero_saques": 0}
         contas_banco[usuario_com_conta - 1][cpf].append(conta_criada)
+        return conta_criada["agencia_conta"]
 
 def depositar(cpf, agencia_conta, deposito, indice_conta, /):
 
     for conta in contas_banco[indice_conta - 1][cpf]:
         if agencia_conta in conta.values():
             conta['saldo'] += deposito
-            conta['extrato'] += f"Depósito de R$ {deposito:.2f}\n"
+            conta['extrato'] += f"    Depósito de R$ {deposito:.2f}\n"
             pass
 
 def sacar(*, cpf, agencia_conta, saque, indice_conta):
@@ -81,7 +83,7 @@ def sacar(*, cpf, agencia_conta, saque, indice_conta):
                     print("Valor do saque excede seu saldo. Saque um valor menor")
                 else:
                     conta["saldo"] -= saque   
-                    conta['extrato'] += f"Saque de R$ {saque:.2f}\n"
+                    conta['extrato'] += f"    Saque de R$ {saque:.2f}\n"
                     conta["numero_saques"] += 1
                     pass
 
@@ -89,30 +91,38 @@ def exibir_extrato(usuario, cpf, indice_conta):
 
     print("<---<--<-< Extrato Bancário >->-->--->")
     print(f"\nNome: {usuario[cpf]["nome"]}")
-    print("\tContas: ")
+    print("Contas: ")
     
     for conta in contas_banco[indice_conta - 1][cpf]:
-        print(f"\t\tAngêcia: {conta["agencia_conta"][0]}")
-        print(f"\t\tConta: {conta["agencia_conta"][1]}")
-        print("\t\t", conta["extrato"])
-        print(f"\t\tSaldo: R${conta["saldo"]}")
+        print(f"   Angêcia: {conta["agencia_conta"][0]}")
+        print(f"   Conta: {conta["agencia_conta"][1]}")
+        print(conta["extrato"])
+        print(f"    Saldo: R$ {conta["saldo"]:.2f}\n")
 
-    print("<-----<----<---< -- >--->---->----->")
+    print("<-----<----<---< ---- >--->---->----->")
+
+def verificar_cadastro_banco_conta(cpf):
+    usuario = procurar_usuario(cpf)
+    usuario_com_conta = procurar_conta(cpf)
+    if not usuario:
+        print("Você ainda não tem cadastro em nosso banco. Cadastre-se antes de fazer movimentações.")
+        return 0, 0
+    elif not usuario_com_conta:
+        print("Você ainda não tem uma conta cadastrada. Crie uma conta antes de fazer movimentações.")
+        return 0, 0
+    else:
+        return usuario, usuario_com_conta
 
 
 while True:
     opcao = input(menu)
 
     if opcao == 'd':
-        cpf = str(input("Digite o CPF do titular da conta: "))
+        cpf = str(input("\nDigite o CPF do titular da conta: "))
 
-        usuario = procurar_usuario(cpf)
-        usuario_com_conta = procurar_conta(cpf)
-        if not usuario:
-            print("Você ainda não tem cadastro em nosso banco. Cadastre-se antes de criar uma conta.")
-
-        elif not usuario_com_conta:
-            print("Você ainda não tem uma conta cadastrada. Crie uma conta antes de fazer movimentações.")
+        usuario, usuario_com_conta = verificar_cadastro_banco_conta(cpf)
+        if not usuario and not usuario_com_conta:
+            pass
         
         else:
             numero_conta = str(input("Digite o número da conta: "))
@@ -122,16 +132,12 @@ while True:
             print("Depósito realizado com sucesso!")
         
     elif opcao == 's':
-        cpf = str(input("Digite o CPF do titular da conta: "))
+        cpf = str(input("\nDigite o CPF do titular da conta: "))
 
-        usuario = procurar_usuario(cpf)
-        usuario_com_conta = procurar_conta(cpf)
-        if not usuario:
-            print("Você ainda não tem cadastro em nosso banco. Cadastre-se antes de criar uma conta.")
-
-        elif not usuario_com_conta:
-            print("Você ainda não tem uma conta cadastrada. Crie uma conta antes de fazer movimentações.")
-
+        usuario, usuario_com_conta = verificar_cadastro_banco_conta(cpf)
+        if not usuario and not usuario_com_conta:
+            pass
+        
         else:
             numero_conta = str(input("Digite o número da conta: "))
             agencia_conta = ("0001", numero_conta)
@@ -143,23 +149,17 @@ while True:
                 print("Saque realizado com sucesso!")
 
     elif opcao == 'e':
-        cpf = str(input("Digite o CPF do titular da conta: "))
+        cpf = str(input("\nDigite o CPF do titular da conta: "))
 
-        usuario = procurar_usuario(cpf)
-        usuario_com_conta = procurar_conta(cpf)
-        if not usuario:
-            print("Você ainda não tem cadastro em nosso banco. Cadastre-se antes de criar uma conta.")
+        usuario, usuario_com_conta = verificar_cadastro_banco_conta(cpf)
+        if not usuario and not usuario_com_conta:
             pass
-
-        elif not usuario_com_conta:
-            print("Você ainda não tem uma conta cadastrada. Crie uma conta antes de fazer movimentações.")
-            pass
-
+        
         else:
             exibir_extrato(usuario, cpf, usuario_com_conta)
 
     elif opcao == 'u':
-        cpf = str(input("Digite o seu CPF: "))
+        cpf = str(input("\nDigite o seu CPF: "))
 
         usuario = procurar_usuario(cpf)
         if not usuario:
@@ -171,15 +171,15 @@ while True:
             print(f"\nOlá, {usuario[cpf]["nome"].split(" ")[0]}, você já está cadastrado em nosso banco. Seja bem-vindo de volta!")
             
     elif opcao == 'c':
-        cpf = str(input("Qual o CPF do titula da conta: "))
+        cpf = str(input("\nQual o CPF do titula da conta: "))
 
         usuario = procurar_usuario(cpf)
         if not usuario:
             print("Você ainda não tem cadastro em nosso banco. Cadastre-se antes de criar uma conta.")
 
         else:
-            cadastrar_conta(cpf)
-            print("Conta criada com sucesso. Você já pode começar a fazer suas transações.")
+            agencia_conta = cadastrar_conta(cpf)
+            print(f"Conta criada com sucesso. Você já pode começar a fazer suas transações.\nAgência: {agencia_conta[0]} Conta: {agencia_conta[1]}")
 
     elif opcao == 'q':
         print("Ecerrando sessão...")
